17fd7de7ffcd41e9e868b19db5f13807bf27ac60
fastboy <giagiang200@gmail.com>
2023-12-21 12:02:41 +0700

    create call to action

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,7 +5,7 @@ const Clients = () => (
   <section className={`${styles.flexCenter} mt-4 `}>
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map((client) => (
-        <div key={client.id} className={`flex-1 ${styles.flexCenter}`}>
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm: min-w-[192px] min-w-[120px]`}>
           <img
             src={client.logo}
             alt="client"
