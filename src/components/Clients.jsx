6aca6f80695aebb7c2ef895056629e87d668a90f
fastboy <giagiang200@gmail.com>
2023-12-21 11:29:41 +0700

    update-feedback section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
+const Clients = () => (
+  <section className={`${styles.flexCenter} mt-4 `}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter}`}>
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
-
-export default Clients
+export default Clients;
